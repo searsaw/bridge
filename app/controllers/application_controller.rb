e03f4d7fbaec7b1c8bfd22acf5fe4c00374e405b
Alex Sears <alexwsears@gmail.com>
2015-03-07 13:42:05 -0500

    Added dashboard nav link and fixed title

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,15 +9,15 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
-    dashboard_index_path
+    dashboard_setup_path
   end
 
   def after_update_path_for(resource)
-    dashboard_index_path
+    dashboard_setup_path
   end
 
   def after_resetting_password_path_for(resource)
-    dashboard_index_path
+    dashboard_setup_path
   end
 
   protected
